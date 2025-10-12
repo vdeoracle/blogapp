b7754ba6c88fb4bfdeec1bd23461be335c59c83d
mr-oracle <vdeoracle@gmail.com>
2025-10-12 13:23:42 -0700

    second commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from app.models import Post
+
 # Create your views here.
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    context = {'post':post}
+    return render(request, 'app/post.html', context)
